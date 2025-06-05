875ef032107bff0f992003a590e342755b235f2d
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-06-05 12:45:25 +0200

    246 interacting with the form object

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {afterNextRender, Component, DestroyRef, inject, viewChild} from '@angular/core';
 import {FormsModule, NgForm} from "@angular/forms";
+import {debounceTime} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,24 @@ import {FormsModule, NgForm} from "@angular/forms";
   ]
 })
 export class LoginComponent {
+
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(()=>{
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value)=>{
+          window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}));
+        },
+      });
+
+      this.destroyRef.onDestroy(()=>subscription?.unsubscribe);
+
+    });
+  }
+
+
   onSubmit(formData: NgForm) {
 
     if (formData.form.invalid){
@@ -21,5 +40,6 @@ export class LoginComponent {
     const password = formData.value.password;
 
     console.log(email, password);
+    formData.form.reset();
   }
 }
