b32133f6878305a708695b7f99d66bf8707fd353
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-06-05 18:03:01 +0200

    255 my sloution

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
-export class SignupComponent {}
+export class SignupComponent {
+
+  signupForm =  new FormGroup({
+    email: new FormControl('',{
+      validators:[Validators.required, Validators.email],
+    }),
+    password: new FormControl('',{
+      validators:[Validators.required, Validators.minLength(6)],
+    }),
+  })
+
+  onSubmit() {
+    const email= this.signupForm.value.email;
+    const password = this.signupForm.value.password;
+    console.log(email,password);
+    this.signupForm.reset();
+  }
+
+  onReset() {
+    this.signupForm.reset();
+  }
+}
